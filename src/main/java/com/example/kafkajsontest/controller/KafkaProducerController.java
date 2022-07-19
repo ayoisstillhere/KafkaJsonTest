5445d51dc360b7a85367621cc70380b1b21b15c2
Ayodele Fagbami <afagbami@gmail.com>
2022-07-19 12:03:48 +0100

    completed controller

--- src/main/java/com/example/kafkajsontest/controller/KafkaProducerController.java
+++ src/main/java/com/example/kafkajsontest/controller/KafkaProducerController.java
@@ -1,2 +1,22 @@
-package com.example.kafkajsontest.controller;public class KafkaProducerController {
+package com.example.kafkajsontest.controller;
+
+import com.example.kafkajsontest.model.User;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+public class KafkaProducerController {
+
+    @PostMapping(value = "/createUser")
+    public void sendMessageToKafkaTopic(
+            @RequestParam("userId") long userId,
+            @RequestParam("firstName") String firstName,
+            @RequestParam("lastName") String lastName
+    ) {
+            User user = new User();
+            user.setUserId(userId);
+            user.setFirstname(firstName);
+            user.setLastName(lastName);
+
+            this.producerService.saveCreateUserLog(user);
+    }
 }
