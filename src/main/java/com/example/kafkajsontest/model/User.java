d765e1681e5f60e310d3729088f31324578122a8
Ayodele Fagbami <afagbami@gmail.com>
2022-07-19 11:53:38 +0100

    Completed user class

--- src/main/java/com/example/kafkajsontest/model/User.java
+++ src/main/java/com/example/kafkajsontest/model/User.java
@@ -1,2 +1,40 @@
-package com.example.kafkajsontest.model;public class User {
+package com.example.kafkajsontest.model;
+
+public class User {
+    private long userId;
+    private String firstname;
+    private String lastName;
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId=" + userId +
+                ", firstname='" + firstname + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
 }
