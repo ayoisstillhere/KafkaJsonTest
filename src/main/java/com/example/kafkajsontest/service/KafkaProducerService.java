83b81348d764fd42d96480a04531df06648e3f0f
Ayodele Fagbami <afagbami@gmail.com>
2022-07-19 12:13:16 +0100

    Completed service

--- src/main/java/com/example/kafkajsontest/service/KafkaProducerService.java
+++ src/main/java/com/example/kafkajsontest/service/KafkaProducerService.java
@@ -1,2 +1,15 @@
-package com.example.kafkajsontest.service;public class KafkaProducerService {
+package com.example.kafkajsontest.service;
+
+import com.example.kafkajsontest.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+
+public class KafkaProducerService {
+
+    @Autowired
+    private KafkaTemplate<String, Object> kafkaTemplate;
+
+    public void saveCreateUserLog(User user){
+        this.kafkaTemplate.send("ayodele", user);
+    }
 }
