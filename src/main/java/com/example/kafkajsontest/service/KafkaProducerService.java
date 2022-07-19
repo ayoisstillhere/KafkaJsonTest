8481edf7a9e53c1ab241cf4f3aa4e4ab3ed3d84c
Ayodele Fagbami <afagbami@gmail.com>
2022-07-19 12:48:26 +0100

    added @service annotation and used a new topic
    Creates topics on the fly

--- src/main/java/com/example/kafkajsontest/service/KafkaProducerService.java
+++ src/main/java/com/example/kafkajsontest/service/KafkaProducerService.java
@@ -3,13 +3,15 @@ package com.example.kafkajsontest.service;
 import com.example.kafkajsontest.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
 
+@Service
 public class KafkaProducerService {
 
     @Autowired
     private KafkaTemplate<String, Object> kafkaTemplate;
 
     public void saveCreateUserLog(User user){
-        this.kafkaTemplate.send("ayodele", user);
+        this.kafkaTemplate.send("newtop", user);
     }
 }
