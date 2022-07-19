cfb20c138f95b18151b0d5389323c4b2b5bcd710
Ayodele Fagbami <afagbami@gmail.com>
2022-07-19 11:49:26 +0100

    initial commit

---
+++ src/test/java/com/example/kafkajsontest/KafkaJsonTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kafkajsontest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaJsonTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
